0ffe1c848298130595789e9d8200c0d54e492b33
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2023-01-22 12:30:40 +0000

    Defined routes of api

--- rails_api/RailsApiDemo/config/routes.rb
+++ rails_api/RailsApiDemo/config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace 'api' do
+    namespace 'v1' do
+    resources :posts
+    resources :users
+    end
+  end
+
 end
